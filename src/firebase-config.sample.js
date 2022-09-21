3473a2e6ef3fadf0488e0212ebd068d0b10f0a9e
shubham2924 <metkar9699130773@gmail.com>
2022-09-21 10:41:49 +0530

    update:secured firebase congifurations

---
+++ src/firebase-config.sample.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "YOUR API KEY",
+  authDomain: "YOUR AUTHDOMAIN",
+  projectId: "YOUR PROJECTID",
+  storageBucket: "YOUR STORAGEBUCKET",
+  messagingSenderId: "YOUR ID",
+  appId: "YOUR APPID",
+  measurementId: "YOUR MEASUREMENT ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
